74913bc4c435961b91db1691d18608d474259475
Anderson991288 <68816726+Anderson991288@users.noreply.github.com>
2023-06-16 22:09:31 +0800

    Add files via upload

--- test.m
+++ test.m
@@ -1,9 +1,29 @@
-%号沏試訓練結果
-%使用一個已經訓練好的深度學習模型 (modified net_10mins)對一個包含潮試圖像的路料集 (imdstest)進厅分類
-%然後計算accuracy
+%���հV�m���G
+% testimagepath = fullfile('gray data-900-w over','testing 4type');
+% imdsTest = imageDatastore(testimagepath, 'IncludeSubfolders',true, 'LabelSource','folderNames');
+imdsTest.ReadFcn = @(loc)imresize(imread(loc),inputSize(1:2));
+[Name,probs_1] = classify(modified_net_10mins,imdsTest);
+accuracy = sum(Name == imdsTest.Labels)/numel(imdsTest.Labels);
 
-testimagepath = fullfile ('gray scale-3600', 'testing');
-imdsTest = imace Datastore (tesf magepath, 'Include subfolders',true, "LabelSource' ,'folderNames');
-imdsTest.ReadFcn = @ (loc) imresize (imread (loc) , inputSize (1:2)) ;
-[Name,probs_1] = classify (modified_net_10mins,imdsTest);
-accuracy = sum (Name== imdsTest.Labels)/nume l (imdsTest.Labels);
+N = 1000; % Number of Testing data
+% 
+% % LF = imdsTest.Labels(1:160,1);
+% LN = imdsTest.Labels(1:1000,1);
+% LQ = imdsTest.Labels(1001:2000,1);
+% LS = imdsTest.Labels(2001:2540,1);
+% LV = imdsTest.Labels(2541:3540,1);
+% 
+% % TF = Name(1:160,1);
+% TN = Name(1:1000,1);
+% TQ = Name(1001:2000,1);
+% TS = Name(2001:2540,1);
+% TV = Name(2541:3540,1);
+% 
+% % AN = sum(LN == TN)/numel(LN);
+% AQ = sum(LQ == TQ)/numel(LQ);
+% AS = sum(LS == TS)/numel(LS);
+% AV = sum(LV == TV)/numel(LV);
+% AF = sum(LF == TF)/numel(LF);
+
+plotconfusion(imdsTest.Labels,Name);
+
--- train.m
+++ train.m
@@ -1,12 +1,14 @@
-net = ResNet18;
-opts = trainingOptions ('sgdm
-	'MiniBatchsize', 20,
-	'MaxEpochs',20, ...
-	'Shuffle', 'every-epoch'
-	"InitialLearnRate',0.001
-	'ValidationData', valDS,..
-	"Plots",'training-progress'
-	'ValidationPatience', 3, 'ExecutionEnvironment', 'gpu');
-	
-	
-	modified net 10mins = trainNetwork (trainDs, net, opts);
+ net =Resnet18;
+
+ opts = trainingOptions('sgdm',...
+         'MiniBatchSize',64,...
+         'MaxEpochs',20, ...
+         'Shuffle','every-epoch', ...
+         'InitialLearnRate',0.001,...
+         'ValidationData',valDS,...
+         "Plots",'training-progress',...
+         'ValidationPatience', 3, 'ExecutionEnvironment','gpu');
+
+
+         modified_net_10mins = trainNetwork(trainDS2, net, opts);
+
